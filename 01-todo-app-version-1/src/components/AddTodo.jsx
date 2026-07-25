46c97c51936f83db9c6ecb8588b6be2dae617ac3
SaddamMohammad1 <saddammahmod4@gmail.com>
2026-07-25 16:14:17 +0530

    first commit

---
+++ 01-todo-app-version-1/src/components/AddTodo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import '../App.css';
+
+function AddTodo() {
+  return (
+    <div className="container">
+      <div className="row kg-row">
+        <div className="col-6">
+          <input type="text" />
+        </div>
+        <div className="col-4">
+          <input type="date" />
+        </div>
+        <div className="col-2">
+          <button type="button" className="btn btn-success kg-button">
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddTodo;
