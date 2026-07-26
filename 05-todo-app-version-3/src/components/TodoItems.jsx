8a08cca2b394800e2c24fc21749d31d5a573e489
SaddamMohammad1 <saddammahmod4@gmail.com>
2026-07-26 21:56:23 +0530

    Todo app version 4 using add and delete functionality

---
+++ 05-todo-app-version-3/src/components/TodoItems.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+import styles from './TodoItems.module.css'
+
+function TodoItems({ todoItems, onDeleteClick }) {
+  return (
+    <div className={styles.itemsContainer}>
+      {todoItems.map((item) => (
+        <TodoItem
+          key={item.name}
+          todoName={item.name}
+          todoDate={item.date}
+          onDeleteClick={onDeleteClick}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default TodoItems;
