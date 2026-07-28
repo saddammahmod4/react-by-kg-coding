a034ebbe218a63c22b8443b6635ba380851d60b3
SaddamMohammad1 <saddammahmod4@gmail.com>
2026-07-29 00:00:45 +0530

    Setup componets for social media app

---
+++ 09-social-media-app/src/components/PostList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Post from './Post'
+
+function PostList() {
+  return (
+    <>
+        <Post />
+        <Post />
+        <Post />
+    </>
+  )
+}
+
+export default PostList
