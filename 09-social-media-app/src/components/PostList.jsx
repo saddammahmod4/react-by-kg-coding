10fe95635e1e7f4c1a50b8b54d40f87b786fff31
SaddamMohammad1 <saddammahmod4@gmail.com>
2026-07-30 01:25:58 +0530

    Handle data fetching using useEffect hook

--- 09-social-media-app/src/components/PostList.jsx
+++ 09-social-media-app/src/components/PostList.jsx
@@ -1,24 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import Post from './Post'
 import { PostList as PostListData } from '../store/post-list-store'
 import WelcomeMessage from './WelcomeMessage'
+import LoadingSpinner from './LoadingSpinner'
 
 function PostList() {
 
   const { postList, addInitialPost } = useContext(PostListData)
+  const [fetching, setFetching] = useState(false);
 
-  const handleGetPostsClick = () => {
+  useEffect(() => {
+    setFetching(true)
     fetch("https://dummyjson.com/posts")
     .then((res) => res.json())
     .then((data) => {
       addInitialPost(data.posts);
+      setFetching(false)
     });
-  }
+  }, [])
 
   return (
     <>
-      {postList.length === 0 && <WelcomeMessage onGetPostsClick={handleGetPostsClick} />}
-      {postList.map((post) => (
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {!fetching && postList.map((post) => (
         <Post key={post.id} post={post} />
       ))}
     </>
--- 09-social-media-app/src/components/WelcomeMessage.jsx
+++ 09-social-media-app/src/components/WelcomeMessage.jsx
@@ -1,16 +1,9 @@
 import React from "react";
 
-function WelcomeMessage({ onGetPostsClick }) {
+function WelcomeMessage() {
   return (
     <center className="welcome-message">
       <h1>There are no post</h1>
-      <button
-        type="button"
-        className="btn btn-primary"
-        onClick={onGetPostsClick}
-      >
-        Get Posts From Server
-      </button>
     </center>
   );
 }
