93b4f5dd73b13fe96155d97f73b2a0071392ca87
SaddamMohammad1 <saddammahmod4@gmail.com>
2026-07-31 23:46:35 +0530

    Clock version using useEffect hook

---
+++ 10-bharat-clock-version-2/src/components/ClockSlogan.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ClockSlogan() {
+  return (
+    <p className='lead'>This is the clock that shows the time in Bharat at all times</p>
+  )
+}
+
+export default ClockSlogan
