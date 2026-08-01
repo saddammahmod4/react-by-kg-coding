c3f78e3f0b726c28547a4e617849a9e97dab97bd
SaddamMohammad1 <saddammahmod4@gmail.com>
2026-08-02 00:21:12 +0530

    create route using react router dom

--- 11-social-media-app-use-router/src/main.jsx
+++ 11-social-media-app-use-router/src/main.jsx
@@ -1,9 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import CreatePost from './components/CreatePost.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/create-post",
+    element: <CreatePost />,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 )
